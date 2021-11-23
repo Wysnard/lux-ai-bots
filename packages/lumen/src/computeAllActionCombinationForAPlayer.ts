3f39984805bf16e29e5e6893d4b4e4c9a1fa8eba
Wysnard <vincent.lay77@gmail.com>
2021-11-23 06:08:28 +0100

    update todo

--- packages/lumen/src/computeAllActionCombinationForAPlayer.ts
+++ packages/lumen/src/computeAllActionCombinationForAPlayer.ts
@@ -5,7 +5,7 @@ import * as LuxSDK from "@lux-ai-bots/lux-sdk";
  * @returns {string[]} list of actions
  *
  * @todo Compute all possible action combinations for a given player.
- * @body Take a player and return all possible action combinations that the player can do.
+ * @body Take a player and return all possible action combinations that the player can do. It should be used to compute all possible combination of 2 players.
  */
 export const computeAllActionCombinationForAPlayer = (
   player: LuxSDK.Player
